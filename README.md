0823deeb6dbf77fe42b6c78c90c4feb1bc83843b
Blend3rman <blend3rman@gmail.com>
2022-01-22 21:18:21 +0530

    init repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Google Firebase Realtime Database - Alexa Skill for SmartCube
+
+This is an Alexa skill template that provides a starting point for creating a skill that uses the [Google Firebase Realtime Database](https://firebase.google.com/docs/database) to receive data in it's cloud-hosted, NoSQL database from an Alexa Skill.
+
+### Using this skill
+
+1. If you don't have one already, create an [Amazon Developer account](https://developer.amazon.com/).
+
+2. Use [this guide](https://developer.amazon.com/en-US/docs/alexa/hosted-skills/alexa-hosted-skills-git-import.html#import) to import this skill to your console
+3. In the Alexa developer console, set the skill's invocation name.
+
+4. Test the skill in the Test tab (after building once).
+
+   > **NOTE:** You can test the skill in devices like: Echo Dot, Echo Show, Fire TV, some Fire tablets, and other devices.
+
+### References
+Based on [DabbleLab's video on Alexa-Firebase Integration](https://www.youtube.com/watch?v=M24yMEijRT)
---
+++ lambda/index.js
@@ -0,0 +1,238 @@
+
+const Alexa = require('ask-sdk-core');
+const firebase = require("firebase/app");
+// Import the functions you need from the SDKs you need
+require('firebase/database');
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEcYg9PzkTjkYcMo_NMtKsEpdZqy6cb1s",
+  authDomain: "wellnesshome-19826.firebaseapp.com",
+  databaseURL: "https://wellnesshome-19826-default-rtdb.firebaseio.com",
+  projectId: "wellnesshome-19826",
+  storageBucket: "wellnesshome-19826.appspot.com",
+  messagingSenderId: "198460061416",
+  appId: "1:198460061416:web:6a2392d5841d5c65a67b5d",
+  measurementId: "G-QV9KGZ8Q2Y"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome! Smart Cube is online now!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const PowerOnOffIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PowerOnOffIntent';
+    },
+    async handle(handlerInput) {
+        const powerState = Alexa.getSlotValue(handlerInput.requestEnvelope, 'power');
+        let speakOutput = '';
+        try{
+            firebase.database().goOnline();
+            await database.ref('/ACState').set({
+                AlexaChangedState : true,
+                Power : (powerState.value === 'on'),
+                ACMode: 'Cool',
+                Temperature : 24,
+                FanSpeed : 'Auto'
+            })
+            firebase.database().goOffline();
+            
+            if(powerState) {
+                let resolution = handlerInput.requestEnvelope.request.intent.slots.power.resolutions.resolutionsPerAuthority[0];
+                if (resolution.status.code === "ER_SUCCESS_MATCH") {
+                    speakOutput = `Alright, turning ${powerState} the air conditioner`;
+                }
+            }
+            
+        }catch(e){
+            console.log("Catch logs here: ",e);
+            speakOutput = `There was a problem turning the air conditioner ${powerState}`
+        }
+        console.log("Out of Try Catch");
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const TemperatureChangeIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'TemperatureChangeIntent';
+    },
+    async handle(handlerInput) {
+        const temperature = Alexa.getSlotValue(handlerInput.requestEnvelope, 'temperature');
+        let speakOutput = '';
+        try{
+            firebase.database().goOnline();
+            await database.ref('/ACState').set({
+                AlexaChangedState : true,
+                Power : true,
+                ACMode: 'Cool',
+                Temperature : parseInt(temperature.value),
+                FanSpeed : 'Auto'
+            })
+            firebase.database().goOffline();
+            
+            if(temperature) {
+                let resolution = handlerInput.requestEnvelope.request.intent.slots.power.resolutions.resolutionsPerAuthority[0];
+                if (resolution.status.code === "ER_SUCCESS_MATCH") {
+                    speakOutput = `Alright, setting the air conditioner to ${temperature} degrees`;
+                }
+            }
+            
+        }catch(e){
+            console.log("Catch logs here: ",e);
+            speakOutput = `There was a problem setting the air conditioner to ${temperature} degrees`
+        }
+        console.log("Out of Try Catch");
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        PowerOnOffIntentHandler,
+        TemperatureChangeIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
